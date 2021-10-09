4de0505c4a7523c5ae6afbf7d1ba2fa677adfb78
Edward Prendergast <edward@adaodesign.com>
2021-10-09 18:34:13 +0100

    Fundamentaly works for me now, nice

--- src/zoneparser/core.clj
+++ src/zoneparser/core.clj
@@ -2,26 +2,61 @@
   (:require [instaparse.core :as insta])
   (:gen-class))
 
-(def zonefile
+(def parse
+  "Example response:
+  "
   (insta/parser
-     "S = (record|mx-record)+;
-      record = name <whitespace> ttl <whitespace> internet <whitespace> record-type <whitespace> value <whitespace>
-      mx-record = name <whitespace> ttl <whitespace> internet <whitespace> \"MX\" <whitespace> priority <whitespace> value <whitespace>
-      name = #'[a-zA-Z0-9\\.]+'
+     "S = (soa|record|variable|<comment>)+;
+      record = (normal-record|mx-record)
+      normal-record = name <whitespace> ttl <whitespace> internet <whitespace> record-type <whitespace> value <whitespace>+
+
+      mx-record = name <whitespace> ttl <whitespace> internet <whitespace> record-type <whitespace> priority <whitespace> value <whitespace>+
+      priority = number
+
+      soa =   name <whitespace>
+              ttl <whitespace>
+              \"IN\" <whitespace>
+              \"SOA\" <whitespace>
+              primary-nameserver <whitespace>
+              admin-email <whitespace>
+
+              <\"(\"> <whitespace?> <newline>?
+              serial <whitespace?> <comment?> <whitespace?>
+              refresh <whitespace?> <comment?> <whitespace?>
+              retry <whitespace?>  <comment?> <whitespace?>
+              expire <whitespace?> <comment?> <whitespace?>
+              negative-response-caching-ttl <whitespace?> <comment?> <whitespace?>
+              <\")\"> <whitespace?>
+
+      variable = <\"$\"> name <whitespace> value <whitespace>?
+
+      comment = \";\" (#'.*') <whitespace?>
+
+      nameserver = alphanum
+      primary-nameserver = alphanum
+      admin-email = alphanum
+      newline = \"\\n\"
+      serial = number
+      refresh = number
+      retry = number
+      expire = number
+      negative-response-caching-ttl = number
+
+      name = alphanum
       whitespace = #'\\s+'
       ttl = #'[0-9]+'
       internet = \"IN\"
       record-type = #'[a-zA-Z0-9]+'
-      priority = alphanum
       value = #'.*'
       number = #'[0-9]+'
-      alphanum = #'[a-zA-Z0-9]+'"))
+      alphanum = #'[a-zA-Z0-9\\._\\-]+'"))
 
-;   (zonefile (slurp "a-records.db")
-;   (zonefile (slurp "zome-mx.db")
+;   (parse (slurp "a-records.db"))
+;   (parse (slurp "zone-mx.db"))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Takes a zone file as a filename and prints out the resulting Clojure datastructure"
   [& args]
-  (println (zonefile (slurp "a-records.db"))
+  (println (parse (slurp "example.db"))
+
            ))
